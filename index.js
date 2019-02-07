149f8ee9cd298bff06713aed00f75ef6644821f4
Kash Reid-Bashir <kash@reid-bashir.com>
2019-02-07 17:14:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-var Recipe = {bread: "flour"}
-var newRecipe = bread.assign({olivebread}, "flour")
-delete newRicipe.bread
-newRecipe
-Recipe
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
